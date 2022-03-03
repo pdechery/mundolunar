dde31d930c30379e5b7c3d732fd58da0d8420859
Pierre Dechery <pierre.dechery@gmail.com>
2022-03-03 11:07:25 -0300

    - Alterando engine database

--- mundolunar/settings/production.py
+++ mundolunar/settings/production.py
@@ -1,5 +1,4 @@
 from .base import *
-import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -8,19 +7,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'www.mundolunar.art.br']
 
-# DATABASES = {
-#     'default': {
-#         'CONN_MAX_AGE': 0,
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'HOST': 'ec2-3-220-222-72.compute-1.amazonaws.com',
-#         'NAME': 'd4anc537lg5ruu',
-#         'PASSWORD': '500334bfce12d8103da9a9b395754fb859ea61a112903b098a55690f338cb13f',
-#         'PORT': '5432',
-#         'USER': 'wbmcinujmjabtk'
-#     }
-# }
-
-DATABASES = {'default': dj_database_url.config()}
+DATABASES = {
+    'default': {
+        'CONN_MAX_AGE': 0,
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'ec2-3-220-222-72.compute-1.amazonaws.com',
+        'NAME': 'd4anc537lg5ruu',
+        'PASSWORD': '500334bfce12d8103da9a9b395754fb859ea61a112903b098a55690f338cb13f',
+        'PORT': '5432',
+        'USER': 'wbmcinujmjabtk'
+    }
+}
 
 try:
     from .local import *
