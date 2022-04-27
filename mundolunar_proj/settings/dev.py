582a7070a5d89b32cda742a5e3376def716f0784
Pierre Dechery <pierre.dechery@gmail.com>
2022-04-27 17:59:27 -0300

    - Commitando arquivos settings com variaveis de ambiente

---
+++ mundolunar_proj/settings/dev.py
@@ -0,0 +1,42 @@
+from decouple import config
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'sqlite': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'default': {
+        'CONN_MAX_AGE': 0,
+        "ENGINE": 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': config('DATABASE_NAME'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'PORT': '5432',
+        'USER': config('DATABASE_USER')
+    }
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', True)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-&clhlnl+ku92c&#in-4f(z$u)+pt%(byc(lvd0c6a2h%sjw(p%'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
---
+++ mundolunar_proj/settings/production.py
@@ -0,0 +1,17 @@
+import dj_database_url
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DEBUG = config('DEBUG', False)
+
+ALLOWED_HOSTS = ['.mundolunar.art.br']
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+try:
+    from .local import *
+except ImportError:
+    pass
