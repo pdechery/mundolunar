5011ad7d41e276a2e5d69d29b623e556ca698ce4
Pierre Dechery <pierre.dechery@gmail.com>
2024-10-28 22:02:30 -0300

    Voltando navegação canções

--- mundolunar/views.py
+++ mundolunar/views.py
@@ -5,41 +5,41 @@ from django.http import JsonResponse
 
 def get_links(request, slug):
 
-	'''
-	1) recebe o nome da música
-	2) localiza esse nome num array de nomes de músicas
-	3) retorna o próximo nome e o nome anterior ao passado
-	'''
+  '''
+  1) recebe o nome da música
+  2) localiza esse nome num array de nomes de músicas
+  3) retorna o próximo nome e o nome anterior ao passado
+  '''
 
-	home_page = HomePage.objects.first()
-	song_list = [song.slug for song in home_page.get_children() if song.content_type.name == "song"]
+  home_page = HomePage.objects.first()
+  song_list = [song.slug for song in home_page.get_children() if song.content_type.name == "song"]
 
-	index = song_list.index(slug)
-	proximas = song_list[index+1:]
-	anteriores = song_list[:index]
+  index = song_list.index(slug)
+  proximas = song_list[index+1:]
+  anteriores = song_list[:index]
 
-	proxima = anterior = 0
+  proxima = anterior = 0
 
-	if(len(proximas) >= 2):
-		proxima = proximas[1]
+  if(len(proximas) >= 1):
+    proxima = proximas[0]
 
-	if(len(anteriores) >= 1):
-		anterior = anteriores[len(anteriores) - 1]
+  if(len(anteriores) >= 1):
+    anterior = anteriores[len(anteriores) - 1]
 
-	song = Page.objects.filter(slug=slug).first()
-	data = song.specific.data_gravacao
+  song = Page.objects.filter(slug=slug).first()
+  data = song.specific.data_gravacao
 
-	return JsonResponse({
-		'soundcloud_id': song.specific.soundcloud_id,
-		'title': song.title,
-		'proxima': proxima,
-		'anterior': anterior,
-		'data_gravacao':data.strftime("%d/%m/%Y"),
-		'equipamentos':song.specific.equipamentos,
-		'instrumentos':song.specific.instrumentos,
-		'autoria':song.specific.autoria,
-		'clima':song.specific.clima,
-	}, safe=False)
+  return JsonResponse({
+    'soundcloud_id': song.specific.soundcloud_id,
+    'title': song.title,
+    'proxima': proxima,
+    'anterior': anterior,
+    'data_gravacao':data.strftime("%d/%m/%Y"),
+    'equipamentos':song.specific.equipamentos,
+    'instrumentos':song.specific.instrumentos,
+    'autoria':song.specific.autoria,
+    'clima':song.specific.clima,
+  }, safe=False)
 
 
 
